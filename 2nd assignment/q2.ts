f7af4b127fdc59e1df6b9e06409f0cba862e01ca
divi-0105 <88383421+divi-0105@users.noreply.github.com>
2021-08-03 21:21:18 +0530

    Add files via upload

---
+++ 2nd assignment/q2.ts
@@ -0,0 +1,19 @@
+      
+     var grade = prompt("Enter a Grade :");  
+     switch (grade) {  
+      case 'A':  
+       alert("Excellent");  
+       break;  
+       case 'B':  
+       alert( "Good");  
+       break;  
+      case 'C':  
+       alert( "Fair");  
+       break;  
+      case 'D':  
+       alert("Poor");  
+       break;  
+      default:  
+       alert("Wrong Grade.........");   
+   }  
+   
---
+++ 2nd assignment/q3.js
@@ -0,0 +1,7 @@
+var name_array1 = ["Amar", "Raja", "Rani"];
+var score_array = ["Chennai", "Delhi", "Banglore"];
+document.write('<table  border="1" cellspacing="0" cellpadding="5"><tr><th>Name</th><th>Place</th></tr>');
+for (var i = 0; i < name_array1.length; i++) {
+    document.write("<tr><td>" + name_array1[i] + "</td><td>" + score_array[i] + "</td></tr>");
+}
+document.write("</table>");
